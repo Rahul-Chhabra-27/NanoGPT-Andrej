d3d3e848a79f44b6244739addbd0010f6bbc4452
Rahul-Chhabra-27 <chhabrarahul2707@gmail.com>
2026-01-31 00:19:13 +0530

    tokens updated..

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ init_from = 'gpt2' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. '
 out_dir = 'out' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
+max_new_tokens = 2 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
@@ -84,6 +84,8 @@ x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 with torch.no_grad():
     with ctx:
         for k in range(num_samples):
+            peak_mem = torch.cuda.max_memory_allocated() / 1024**2
+            print("Peak GPU memory (MB):", peak_mem)
             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
             print(decode(y[0].tolist()))
             print('---------------')
